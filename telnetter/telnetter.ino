1f4781acf3549ed1219edf4fa639a7318f5a4eda
980f <andy.980f@gmail.com>
2018-12-10 15:44:22 -0600

    partitioned telnetter

--- telnetter/telnetter.ino
+++ telnetter/telnetter.ino
@@ -29,51 +29,87 @@ const InputPin<D8, LOW> Initparams;
 
 #include "telnetter.h"
 Credentials cred;
-Telnetter net;
+
 
 #include "nvblock.h"
 
 IPAddress mystaticip(192, 168, 12, 65); //65=='A'
 const unsigned CredAddress = 0;
 
-const Nvblock keepip=Nvblock::For(mystaticip,128);//todo: allocation scheme to ensure no overlap, without wrapping with a struct.
+const Nvblock keepip = Nvblock::For(mystaticip, 128); //todo: allocation scheme to ensure no overlap, without wrapping with a struct.
+
+const char *Hostname = "980FMesher";
+
+struct Chatter: public TelnetActor {
+  bool chat = true;
+  void onInput(uint8_t *bytes, unsigned length, unsigned ci);
+  void onConnect(WiFiClient &aclient, unsigned ci);
+  const char *hostname() ;
+} chat;
+
+Telnetter net(&chat);
+
+void Chatter::onInput(uint8_t *bytes, unsigned length, unsigned ci) {
+  Serial.write(bytes, length);
+  if (chat) {
+    net.broadcast(*bytes, length, ci);
+  }
+}
+
+void Chatter::onConnect(WiFiClient &aclient, unsigned ci) {
+  aclient.write(Hostname);
+  aclient.write(" at your service, station ");
+  aclient.write('0' + ci);
+  aclient.write(".\n");
+}
+
+const char *Chatter::hostname() {
+  return Hostname;//todo:eeprom
+}
 
 void setup() {
   Serial.begin(115200);
 
   EEPROM.begin(4096);//4096:esp8266 max psuedo eeprom
+  cred.load(CredAddress);
+
+  if(Verbose){
+    dbg("\nStored Credentials:",cred.ssid,cred.password);
+  }
+      
   if (Initparams) {
     cred.setID("honeypot");
     cred.setPWD("brigadoon-will-be-back-soon");
-    
+
     cred.save(CredAddress);
     keepip.save();
   } else {
-    cred.load(CredAddress);
-    keepip.load();    
+//    cred.load(CredAddress);
+    keepip.load();
   }
-  
+
   net.begin(cred);
 }
+
 
 void loop() {
   if (MilliTicked) { //must be polled for MonoStables to work
     //by only checking once per millisecond we lower total power consumption a smidgeon.
-    if (net.serve(Serial)) {
+    if (net.serve()) {
       net.broadcast(Serial);
     } else {
       if (size_t len = Serial.available()) {
         char sbuf[len + 1]; //not standard C++! (but most compilers will let you do it)
         Serial.readBytes(sbuf, len);
         sbuf[len] = 0; //avert buffer overflow
-        unsigned colonated = strztok(sbuf, len, ':');
-        if (colonated != ~0U) {
-          if (colonated++) { //then we have a ssid
+
+        unsigned passat = strztok(sbuf, len, '@');
+        if (passat != ~0U) {
+          if (passat++) { //then we have a ssid in front, the ++ skips over the '@'
             cred.setID(sbuf);
           }
-          if (sbuf[colonated]) { //have more bytes?
-            cred.setPWD(&sbuf[colonated]);
-          }
+          //a trailing '@' means no password.
+          cred.setPWD(&sbuf[passat]);
         }
       }
     }
