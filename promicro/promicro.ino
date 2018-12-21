68eb596609bfa5fc1c077cef329c8e3007425314
980f <andy.980f@gmail.com>
2018-12-20 22:33:11 -0600

    L298 version, same speed limit, the motor ain't the problem

--- promicro/promicro.ino
+++ promicro/promicro.ino
@@ -84,20 +84,35 @@ const InputPin<15> slowerButton;
 const InputPin<16> thisButton;
 const InputPin<14> thatButton;
 
-
+static const unsigned grey2[] = {0, 1, 3, 2};
 class Stepper {
+
   public:
+    enum Iface {Uni4 = 0, Bip4 = 1}; //we are slow enough to not want to pay extra for virtual function call.
+    Iface iface = Bip4; //
     int step = 0;
     //  unsigned perRevolution=200;
     //  unsigned phase=0;
     //
-    void applyPhase(unsigned phase) {
-      unsigned bits = 0x33 >> (phase % 4);
 
-      ph0 = bitFrom(bits, 0);
-      ph1 = bitFrom(bits, 1);
-      ph2 = bitFrom(bits, 2);
-      ph3 = bitFrom(bits, 3);
+    void applyPhase(unsigned phase) {
+      switch (iface) {
+        case Uni4: {
+            unsigned bits = 0x33 >> (phase % 4);
+            ph0 = bitFrom(bits, 0);
+            ph1 = bitFrom(bits, 1);
+            ph2 = bitFrom(bits, 2);
+            ph3 = bitFrom(bits, 3);
+          }
+          break;
+        case Bip4: {
+            unsigned bits = grey2[phase & 3]; //Gray code count,
+            ph0 = bitFrom(bits, 0);
+            ph1 = bitFrom(~bits, 0);
+            ph2 = bitFrom(bits, 1);
+            ph3 = bitFrom(~bits, 1);
+          } break;
+      }
     }
 
     operator ()(bool fwd) {
