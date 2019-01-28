f536a5b7d5c390d2c45548af7a737c36f5c238e6
980f <andy.980f@gmail.com>
2019-01-28 12:09:52 -0600

    excised I2C bus scan

--- beholder/beholder.ino
+++ beholder/beholder.ino
@@ -41,32 +41,7 @@ ProMicro board;
 #include "pca9685.h"
 PCA9685 pwm;
 
-//diagnostic, should go in own file.
-void scanI2C() {
-  Console("\nScanning for I2C devices:");
-  for (byte address = 16; address < 123; address++ )  {
-    Wire.beginTransmission(address);
-    auto error = Wire.endTransmission();
-    switch (error) {
-      case 0://success
-        Console("\nI2C device found at address ", address);
-        break;
-      case 1://data too long to fit in transmit buffer
-        break;
-      case 2://received NACK on transmit of address
-        break;
-      case 3://received NACK on transmit of data
-        break;
-      case 4://other error
-        Console("\nUnknown error for address ", address);
-        break;
-      default:
-        Console(".");
-        break;
-    }
-  }
-  Console("\nScanning for I2C devices is done.");
-}
+#include "scani2c.h"
 
 //joystick to servo value:
 #include "analog.h"
