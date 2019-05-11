34d133f22d8effc5fcae54ad837fc34a0792b412
980f <andy.980f@gmail.com>
2019-05-10 23:38:44 -0500

    must use D3 instead of 3 with espresso arduino, they enum'd what others indexed into an array for

--- clockolanthe/clockolanthe.ino
+++ clockolanthe/clockolanthe.ino
@@ -21,39 +21,49 @@ int spinit = 0;
 
 //OutputPin<2> led;
 
-void step(){	
+void step() {
   positioner += spinit;
-//	led=(positioner.step&3) == 3;
+  //	led=(positioner.step&3) == 3;
 }
 
 
 void upspeed(unsigned newspeed) {
   if (changed(thespeed, newspeed)) {
-		ticker.set(thespeed);//this one will stretch a cycle in progress. 
-    dbg("\nSpeed:",thespeed);
+    ticker.set(thespeed);//this one will stretch a cycle in progress.
+    dbg("\nSpeed:", thespeed);
   }
 }
+
+const OutputPin<D4> xph0;
+const OutputPin<D3> xph1;
+const OutputPin<D2> xph2;
+const OutputPin<D1> xph3;
+
 
 /** keytroke commands for debugging stepper motors */
 bool stepCLI(char key) {
   switch (key) {
     case ' ':
-      dbg("\nSpeed:",thespeed," \nLocation:",positioner);
+      dbg("\nSpeed:", thespeed, " \nLocation:", positioner, "\nPhases:", xph0, xph1, xph2, xph3);
       break;
-
+    case 'i':
+      upspeed(thespeed);//start timer, but the load won't move unless we default spinit to non-zero.
+      dbg("\npinsymbols:", D1, D2, D3, D4);
+      break;
     case '1': case '2': case '3': case '4': //jump to phase
       positioner.applyPhase(key - 1);
+      dbg("\nPhases:", xph0, xph1, xph2, xph3);
       break;
     case 'q'://zero counter while retaining phase.
       positioner.step &= 3; //closest to 0 we can get while the phases are still tied to position.
       break;
     case 'w'://manual step forward
       ++positioner;
-//      dbg.println(positioner.step);
+      dbg("\nPhases:", xph0, xph1, xph2, xph3);
       break;
     case 'e'://manual step back
       --positioner;
-//      dbg.println(positioner.step);
+      dbg("\nPhases:", xph0, xph1, xph2, xph3);
       break;
     case 'x'://stop stepping
       spinit = 0;
@@ -82,18 +92,18 @@ bool stepCLI(char key) {
 }
 
 void setup() {
-	Serial.begin(115200);
-	upspeed(thespeed);//start timer, but the load won't move unless we default spinit to non-zero.
+  Serial.begin(115200);
+
 }
 
 void loop() {
-	if(MilliTicked){
-  	while(Serial.available()>0){//only checking every milli to save power
-  		stepCLI(Serial.read());
-  	}
-  	if(ticker.perCycle()){
-  		step();
-  	}
-	}
+  if (MilliTicked) {
+    while (Serial.available() > 0) { //only checking every milli to save power
+      stepCLI(Serial.read());
+    }
+    if (ticker.perCycle()) {
+      step();
+    }
+  }
 
 }
