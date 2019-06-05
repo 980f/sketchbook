c3e12c3f17396ecd9afd72d25b68c4b9d384fd28
980f <andy.980f@gmail.com>
2019-06-05 18:12:57 -0500

    splitting out pieces, getting processor switching automated

---
+++ clockolanthe/clockdriver.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "hms.h" //HoursMinutesSeconds, not the Pinafore.
+class ClockDriver {
+  public:
+    HMS target;//intermediary for RPN commands
+    ClockDriver();
+
+    void runReal(bool jerky);
+    void setMinute(unsigned arg,bool raw=false);
+    void setHour(unsigned arg,bool raw=false);
+//emit debug information to its own logger
+    void dump();
+};
