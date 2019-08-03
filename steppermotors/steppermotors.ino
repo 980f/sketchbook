8d2c0876b898fcefd64d7b877d6a5c9581833809
980f <andy.980f@gmail.com>
2019-08-03 17:06:46 -0500

    ready for test of code

--- steppermotors/steppermotors.ino
+++ steppermotors/steppermotors.ino
@@ -1,7 +1,18 @@
 
 #include "options.h"
+/* test stepper motors.
+    started with code from a clock project, hence some of the peculiar naming and the use of time instead of degrees.
 
+    the test driver initially is an adafruit adalogger, it is what I had wired up for the clock.
+    the test hardware has a unipolar/bipolar select on D17
+    and power on/off D18 (need to learn the polarity )
 
+    it is convenient that both DRV8333 and L298n take the same control input, and that pattern is such that a simple connect of the center taps to the power supply makes them operate as unipolar.
+
+    with fixed power supply voltage the unipolar mode is twice the voltage on half the coil.
+    the uni current is double, but only half as many coils are energized.
+
+*/
 ////////////////////////////////////
 #include "pinclass.h"
 #include "cheaptricks.h"
@@ -16,7 +27,6 @@ Using_MilliTicker
 
 #include "stepper.h"
 #include "motordrivers.h"
-#include "clockhand.h"
 
 //project specific values:
 const unsigned baseSPR = 2048;//28BYJ-48
@@ -24,27 +34,24 @@ const unsigned slewspeed = 5;//5: smooth moving, no load.
 
 
 
-#if UsingDRV8833
 
 #ifdef ADAFRUIT_FEATHER_M0
-DRV8833<10, 9, 12, 11, 13> minutemotor;
-DRV8833< 16, 15, 17, 18, 14> hourmotor;
-//void hourmotor(byte step){}
+FourBanger<10, 9, 12, 11> minutemotor;
+OutputPin<17> unipolar;//else bipolar
+OutputPin<18> motorpower;//relay, don't pwm this!
 #else  //presume ProMicro/Leonardo
-DRV8833<12, 11, 10, 9, 13> minutemotor;
-DRV8833<16, 17, 5, 18, 19> hourmotor;
+FourBanger<12, 11, 10, 9> minutemotor;
+bool unipolar;
+bool motorpower;//todo: assign real pins
+#endif
 
-#endif
-#else
-//#error "must define one of the driver classes such as UsingUDN2540, UsingDRV8833 ..."
-void minutemotor(byte step) {}
-void hourmotor(byte step) {}
-#endif
 
 ///////////////////////////////////////////////////////////////////////////
 #include "char.h"
-char steptrace[32 + 1];
+char steptrace[32 + 1];//debug
 unsigned stepcount = 0;
+
+#include "clockhand.h"
 
 ClockHand minuteHand(ClockHand::Minutes, [](byte step) {
   minutemotor(step);
@@ -52,9 +59,6 @@ ClockHand minuteHand(ClockHand::Minutes, [](byte step) {
   stepcount %= 32;
 });
 
-ClockHand hourHand(ClockHand::Hours, [](byte step) {
-  hourmotor(step);
-});
 
 
 template <PinNumberType forward, PinNumberType reverse> class SlewControl {
@@ -68,7 +72,6 @@ template <PinNumberType forward, PinNumberType reverse> class SlewControl {
     }
 
     void onTick() { //every millisecond
-      //      if (myHand) {
       if (fwd) {
         myHand.freerun = +1;
         myHand.upspeed ( slewspeed);
@@ -78,83 +81,26 @@ template <PinNumberType forward, PinNumberType reverse> class SlewControl {
       } else {
         myHand.realtime();
       }
-      //      }
     }
 };
 
-SlewControl<20, 21> minuteSlew(minuteHand);
-SlewControl<23, 24> hourSlew(hourHand);
+//if adalogger:
+SlewControl<23, 20> minuteSlew(minuteHand);
 
-//arduino has issues with a plain function name matching that of a class in the same file!
-void upspeedBoth(unsigned msperstep) {
-  minuteHand.upspeed(msperstep);
-  hourHand.upspeed(msperstep);
-}
-
-void freezeBoth() {
-  minuteHand.freeze();
-  hourHand.freeze();
-}
 
 //now is reference position. Might be 5:00 a.m. for act II of Iolanthe
 void highnoon() {
   minuteHand.setReference(0);
-  hourHand.setReference(0);
 }
 
-
-
-#include "clockdriver.h"
-
-//interface to local logic
-ClockDriver::ClockDriver():
-  target(0)
-{
-  //#done.
-}
-
-void ClockDriver::runReal() {
-  minuteHand.realtime();
-  hourHand.realtime();
-}
-
-void ClockDriver::setMinute(unsigned arg, bool raw) {
-  if (raw) {
-    dbg("\nMinute step to:", arg);
-    minuteHand.setTarget(arg);
-  } else {
-    dbg("\nMinute time to:", arg);
-    minuteHand.setTime(target.minute = arg);
-  }
-}
-
-void ClockDriver::setHour(unsigned arg, bool raw) {
-  if (raw) {
-    dbg("\nHour stepping to:", arg);
-    hourHand.setTarget(arg);
-  } else {
-    dbg("\nHour time to:", arg);
-    hourHand.setTime(target.hour = arg);
-  }
-}
 
 void reportHand(const ClockHand&hand, const char *which) {
   dbg("\n", which, "T=", hand.target, " en:", hand.enabled, " FR=", hand.freerun, " Step=", hand.mechanism);
 }
 
-
-void ClockDriver::dump() {
-  reportHand(hourHand, "Hour");
-  reportHand(minuteHand, "Minute");
-}
-
-ClockDriver bigben;
-
 //command line interpreter, up to two RPN unsigned int arguments.
 #include "clirp.h"
 CLIRP cmd;
-
-ClockHand *hand = &minuteHand; //for tweaking one at a time
 
 //I2C diagnostic
 #include "scani2c.h"
@@ -163,29 +109,20 @@ ClockHand *hand = &minuteHand; //for tweaking one at a time
 void doKey(char key) {
   switch (key) {
     case 'T': case 't': //run real time, smoothly
-      bigben.runReal();
+      minuteHand.realtime();
       break;
 
     case ' '://report status
-      bigben.dump();
-
+      reportHand(minuteHand, "Minute");
       dbg("\nTrace:", steptrace);
       break;
 
-    case 'h'://go to position
-      bigben.setHour(cmd.arg, true);
-      break;
-
     case 'm'://go to position
-      bigben.setMinute(cmd.arg, true);
-      break;
-
-    case ':':
-      bigben.setHour(cmd.arg);
+      minuteHand.setTarget(cmd.arg);
       break;
 
     case ';':
-      bigben.setMinute(cmd.arg);
+      minuteHand.setTime(cmd.arg);
       break;
 
     case 'Z'://declare present position is noon
@@ -195,37 +132,33 @@ void doKey(char key) {
 
     case 'v'://set stepping rate to use
       dbg("\nSetting step:", cmd.arg);
-      upspeedBoth(cmd.arg);
+      minuteHand.upspeed(cmd.arg);
       break;
 
-    case 'x'://stop stepping of both
+    case 'x': case 'X': //stop stepping
       dbg("\nStopping.");
-      freezeBoth();
+      minuteHand.freeze();
       break;
 
-    case 'H':
-      dbg("\nfocussing on Hour hand");
-      hand = &hourHand;
+    case 'u': case 'U':
+      dbg("\nunipolar engaged");
+      unipolar = 1;
       break;
-
-    case 'M':
-      dbg("\nfocussing on minute hand");
-      hand = &minuteHand;
+    case 'b': case 'B':
+      dbg("\nbipolar engaged");
+      unipolar = 0;
       break;
 
     case 'R'://free run in reverse
       dbg("\nRun Reverse.");
-      hand->freerun = -1;
+      minuteHand.freerun = -1;
       break;
 
     case 'F'://run forward
       dbg("\nRun Forward.");
-      hand->freerun = +1;
+      minuteHand.freerun = +1;
       break;
 
-    case 'X'://stop just the one
-      hand->freeze();
-      break;
 
     case '?':
       scanI2C(dbg);
@@ -260,26 +193,23 @@ void setup() {
   dbg("setup");
 
   dbg("setup upsspeed");
-  upspeedBoth(slewspeed);
+  minuteHand.upspeed(slewspeed);
   dbg("setup basespr");
   minuteHand.stepperrev = baseSPR;
-  minutemotor.enabler = 1;
+  motorpower = 1;
 
-  hourHand.stepperrev = baseSPR; //todo: pulley ratio goes here
-  hourmotor.enabler = 1;
 
   //here is where we would configure step duration.
-  bigben.runReal(); //power cycle at least gets us moving.
+  minuteHand.realtime(); //power cycle at least gets us moving.
 }
 
 
 void loop() {
   if (MilliTicked) {
     minuteSlew.onTick();
-    hourSlew.onTick();
     //if both need to run, minute gets priority (in case we share control lines)
     if (!minuteHand.onTick()) {
-      hourHand.onTick();
+      //      hourHand.onTick();
     }
     doui();
   }
