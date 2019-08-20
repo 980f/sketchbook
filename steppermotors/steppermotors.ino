841cc69db1953c1ec85cf5d9fdde52f7f25bc01d
980f <andy.980f@gmail.com>
2019-08-20 18:34:13 -0500

    dualbridge code complete.

--- steppermotors/steppermotors.ino
+++ steppermotors/steppermotors.ino
@@ -1,4 +1,5 @@
-#include <Arduino.h>
+#include <Arduino.h> //needed by some IDE's.
+
 #include "options.h"
 /* test stepper motors.
     started with code from a clock project, hence some of the peculiar naming and the use of time instead of degrees.
@@ -16,7 +17,9 @@
 ////////////////////////////////////
 #include "pinclass.h"
 #include "cheaptricks.h"
+
 #include "easyconsole.h"
+
 EasyConsole<decltype(Serial)> dbg(Serial, true /*autofeed*/);
 
 
@@ -25,13 +28,14 @@ EasyConsole<decltype(Serial)> dbg(Serial, true /*autofeed*/);
 
 #include "microevent.h"
 #include "stepper.h"
-#include "motordrivers.h"
-
-//project specific values:
-const unsigned baseSPR = 200;//1.8' steppers ar ethe ones that need speed testing
-
-//todo: code in seconds so that we can switch between micro and milli
-RawMicros slewspeed = 500000;
+#include "motordrivers.h" //FourBanger and similar
+//
+////project specific values:
+////steps per rev matters only when seeking zeroMarker, might be handy for UI.
+//const unsigned baseSPR = 200;//1.8' steppers ar ethe ones that need speed testing
+//
+////todo: code in seconds so that we can switch between micro and milli
+//RawMicros slewspeed = 500000;
 
 
 #ifdef ADAFRUIT_FEATHER_M0
@@ -43,154 +47,191 @@ FourBanger<8, 11, 12, 13> minutemotor;
 bool unipolar;//else bipolar
 DuplicateOutput<9,10> motorpower;//pwm OK. These are the ENA and ENB of the L298, for stepper use there is no reason to just keep one active.
 
-#else  //presume ProMicro/Leonardo
-FourBanger<4,5,6,7> minutemotor;
-bool unipolar;
-bool motorpower;//todo: assign real pins
-#endif
+#else  //presume ProMicro/Leonardo with dual. we'll clean this up someday
 
+#include "spibridges.h"
 
-///////////////////////////////////////////////////////////////////////////
-#include "char.h"
-char steptrace[32 + 1];//debug
-unsigned stepcount = 0;
-
-#include "clockhand.h"
-
-ClockHand minuteHand(ClockHand::Minutes, [](byte step) {
-  minutemotor(step);
-  steptrace[stepcount++] = Char(motorNibble(step)).hexNibble(0);
-  stepcount %= 32;//diagnostic loop, unrelated to stepper action. Used to look for erratic timebase.
-});
-
-
-
-template <PinNumberType forward, PinNumberType reverse> class SlewControl {
-  public: //for diagnostics
-    InputPin<forward> fwd;
-    InputPin<reverse> rev;
-  public: //easier to set after construction.
-    ClockHand &myHand;
-    SlewControl(ClockHand & myHand): myHand(myHand) {
-      //#set myHand on setup()
-    }
-
-    void onTick() { //every millisecond
-      if (fwd) {
-        myHand.freerun = +1;
-        myHand.upspeed ( slewspeed);
-      } else if (rev) {
-        myHand.freerun = -1;
-        myHand.upspeed ( slewspeed);
-      } else {
-        myHand.realtime();
-      }
-    }
-};
-
-//if adalogger:
-SlewControl<23, 20> minuteSlew(minuteHand);
-
-
-//now is reference position. Might be 5:00 a.m. for act II of Iolanthe
-void highnoon() {
-  minuteHand.setReference(0);
+template<bool second> void bridgeLambda(byte phase) {
+  SpiDualBridgeBoard::setBridge(second, phase);
 }
 
-//clion maker can't handle user defined classes in its forward reference generator.
-//void reportHand(const ClockHand&hand, const char *which) {
-//  dbg("\n", which, "T=", hand.target, " en:", hand.enabled, " FR=", hand.freerun, " Step=", hand.mechanism);
-//}
+void bridge0(byte phase) {
+  bridgeLambda<0>(phase);
+}
+
+void bridge1(byte phase) {
+  bridgeLambda<1>(phase);
+}
+
+#endif
+
+/** input for starting position of cycle. */
+InputPin<14> zeroMarker;
+///////////////////////////////////////////////////////////////////////////
+#include "char.h"
+
+/** adds velocity to position.  */
+struct StepperMotor {
+  Stepper pos;
+
+  MicroStable ticker;
+
+  /** if and which direction to step*/
+  int run = 0;//1,0,-1
+  Stepper::Step target = 0;
+  bool freeRun = false;
+
+  void setTick(MicroStable::Tick perstep) {
+    ticker.set(perstep);
+  }
+
+  void operator()() {
+    if (ticker.perCycle()) {
+      if (!freeRun) {
+        run = pos - target;//weird but true, the x= ops are differential, the non x= ops are absolute, and we wish to differential step in a direction determined by absolute position.
+      }
+      pos += run;
+    }
+  }
+
+  void stats(ChainPrinter *adbg) {
+    if (adbg) {
+      (*adbg)("T=", target, " FR=", freeRun, " Step=", int(pos));
+    }
+  }
+
+  /** stop where it is. */
+  void freeze() {
+    freeRun = false;
+    target = pos;
+  }
+
+  /** not actually there until the step has had time to settle down. pessimistically that is when the next step would occur if we were still moving.*/
+  bool there() const {
+    return run == 0 && target == pos;
+  }
+
+  void start(bool second, Stepper::Interface iface) {
+    pos.interface = iface;//lambda didn't work
+//      [second](byte phase){
+//      SpiDualBridgeBoard::setBridge(second,phase);
+//    };
+    setTick(250000);
+  }
+};
+
+StepperMotor motor[2];//setup will attach each to pins/
+
+///* run fast in direction controlled by button. */
+//template <PinNumberType forward, PinNumberType reverse> class SlewControl {
+//  public: //for diagnostics
+//    InputPin<forward> fwd;
+//    InputPin<reverse> rev;
+//  public: //easier to set after construction.
+//    ClockHand &myHand;
+//    SlewControl(ClockHand & myHand): myHand(myHand) {
+//      //#set myHand on setup()
+//    }
+//
+//    void onTick() { //every millisecond
+//      if (fwd) {
+//        myHand.freerun = +1;
+//        myHand.upspeed ( slewspeed);
+//      } else if (rev) {
+//        myHand.freerun = -1;
+//        myHand.upspeed ( slewspeed);
+//      } else {
+//        myHand.realtime();
+//      }
+//    }
+//};
+
 
 //command line interpreter, up to two RPN unsigned int arguments.
 #include "clirp.h"
+
 CLIRP cmd;
 
 //I2C diagnostic
 #include "scani2c.h"
 
-
 void doKey(char key) {
+  bool which = key < 'a';//which of two motors
   switch (key) {
-    case 'T': case 't': //run real time, smoothly
-      minuteHand.realtime();
-      break;
 
-    case ' '://report status
-//clion can't handle this:      reportHand(minuteHand, "Minute");
-			dbg( "T=", minuteHand.target, " en:", minuteHand.enabled, " FR=", minuteHand.freerun, " Step=", minuteHand.mechanism);
-      dbg("Trace:", steptrace);
-      break;
+  case ' '://report status
+    motor[0].stats(&dbg);
+    motor[1].stats(&dbg);
+    break;
 
-    case 'm'://go to position
-      minuteHand.setTarget(cmd.arg);
-      break;
+  case 'm'://go to position
+    motor[which].target = cmd.arg;
+    break;
 
-    case ';':
-      minuteHand.setTime(cmd.arg);
-      break;
+  case 'Z':
+  case 'z': //declare present position is ref
+    dbg("marking start");
+    motor[which].pos = 0;
+    break;
 
-    case 'Z'://declare present position is noon
-      dbg("marking noon");
-      highnoon();
-      break;
+  case 'S':
+  case 's'://set stepping rate to use for slewing
+    dbg("Setting slew:", cmd.arg);
+    motor[which].setTick(cmd.arg);
+    break;
 
-    case 'v'://set stepping rate to use for timekeeping
-      dbg("Setting step:", cmd.arg);
-      minuteHand.upspeed(cmd.arg);
-      break;
+  case 'x':
+  case 'X': //stop stepping
+    dbg("Stopping.");
+    motor[which].freeze();
+    break;
 
-    case 's'://set stepping rate to use for slewing
-      dbg("Setting slew:", cmd.arg);
-      slewspeed = cmd.arg;
-      break;
+    //one test system had two relays for switching the motor wiring:
+    //  case 'u':
+//  case 'U':
+//    dbg("unipolar engaged");
+//    unipolar = true;
+//    break;
+//  case 'b':
+//  case 'B':
+//    dbg("bipolar engaged");
+//    unipolar = false;
+//    break;
 
+  case 'p':
+  case 'P':
+    dbg("power on");
+    SpiDualBridgeBoard::power(which, true);
+    break;
 
-    case 'x': case 'X': //stop stepping
-      dbg("Stopping.");
-      minuteHand.freeze();
-      break;
+  case 'O':
+  case 'o':
+    dbg("power off");
+    SpiDualBridgeBoard::power(which, false);
+    break;
 
-    case 'u': case 'U':
-      dbg("unipolar engaged");
-      unipolar = 1;
-      break;
-    case 'b': case 'B':
-      dbg("bipolar engaged");
-      unipolar = 0;
-      break;
+  case 'R':
+  case 'r'://free run in reverse
+    dbg("Run Reverse.");
+    motor[which].freeRun = -1;
+    break;
 
-    case 'p': case 'P':
-      dbg("power on");
-      motorpower = true;
-      break;
-    case 'o': case 'O':
-      dbg("power off");
-      motorpower = false;
-      break;
+  case 'F':
+  case 'f'://run forward
+    dbg("Run Forward.");
+    motor[which].freeRun = +1;
+    break;
 
-
-    case 'R'://free run in reverse
-      dbg("Run Reverse.");
-      minuteHand.freerun = -1;
-      break;
-
-    case 'F'://run forward
-      dbg("Run Forward.");
-      minuteHand.freerun = +1;
-      break;
-
-
-    case '?':
-      scanI2C(dbg);
+  case '?':
+    scanI2C(dbg);
 #if UsingEDSir
-      dbg("IR device is ", IRRX.isPresent() ? "" : "not", " present");
+    dbg("IR device is ", IRRX.isPresent() ? "" : "not", " present");
 #endif
-      break;
+    break;
 
-    default:
-      dbg("Ignored:", char(key), " (", key, ") ", cmd.arg, ',', cmd.pushed);
-      return; //don't put in trace buffer
+  default:
+    dbg("Ignored:", char(key), " (", key, ") ", cmd.arg, ',', cmd.pushed);
+    return; //don't put in trace buffer
   }
 }
 
@@ -208,27 +249,30 @@ void doui() {
 
 /////////////////////////////////////
 
+
+
+
 void setup() {
-  steptrace[32] = 0; //terminate debug text
   Serial.begin(115200);
   dbg("setup");
+  SpiDualStepper::start(true);//using true during development, low power until program is ready to run
+
+  motor[0].start(0, bridge0);
+
+  motor[1].start(1, bridge1);
 
   dbg("setup upsspeed");
-  minuteHand.upspeed(slewspeed);
-  dbg("setup basespr");
-  minuteHand.stepperrev = baseSPR;
-  motorpower = 1;
-
-  //here is where we would configure step duration.
-  minuteHand.realtime(); //power cycle at least gets us moving.
 }
 
-
-
 void loop() {
-  if (MicroTicked) {
-    minuteSlew.onTick();
-    minuteHand.onTick();
+  if (MicroTicked) {//on avr this is always true, cortexm0, maybe 50%
+    motor[0]();
+    motor[1]();
+  }
+  if (MilliTicked) {//no urgent things like debouncing index sensor
+    if(zeroMarker){
+      motor[0].freeze();
+    }
     doui();
   }
 }
