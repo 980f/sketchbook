3a02be13445e2fe3d18729fc5c1ddcfd6169127c
980f <andy.980f@gmail.com>
2021-10-03 21:16:47 -0500

    need better computer for major editing

--- scanner2021/scanner2021.ino
+++ scanner2021/scanner2021.ino
@@ -32,6 +32,9 @@ bool idleNear;   //if true then idle near the motor, else idle at far end.
 MilliTick outbound;//time from starting moving scanner bar away from motor to stopping.
 MilliTick inbound;//time from starting moving scanner bar towards motor until turn around
 
+//timers for out and in bound moves
+Monostable endrun;// when true quit moving
+Monostable deadtime;// when true start action for current state
 //state of motion
 bool haveHomed=0; //until we have activated home switch we don't know where we are
 bool inMotion=0;   //run relay should be on
@@ -50,16 +53,84 @@ enum MotionState {
 
 //set to non-zero when should be scanning. When decremented to zero motion stops once home.
 unsigned scanpass=0;
+MotionState activity=NeedsHoming;
 
+//we want to avoid current spikes and any jerking from relay response time differenceses
+void enterState(MotionState newstate,MilliTick motiontimeout){
+  scan = 0; //always halt before any potential change of direction. Since most calls change direction we don't bother checking.      
+  deadtime = 50; //todo:  measure worst of motion stop and relay skew.
+  endrun = motiontimeout;
+  activity = newstate;
+}
 
 void setup() {
   //todo: read options from EEprom 
   Serial.begin(115200);
 }
 
-void loop() {
-  
+void loop() {  
   if(MilliTicked){//nothing need be fast and the processor may be in a tight enclosure
+    if(!deadtime.isRunning()){
+        
+      if(endrun){
+        switch(activity){
+          case NeedsHoming:
+            activity=NeedsHoming;
+          break;
+          case HomeAway:
+            scan=0;      
+            deadtime=50;
+            endrun=inbound;
+            activity=HomeBack;
+          break;
+          case HomeBack:
+            activity=;
+          break;
+          case AtHome:
+            activity=;
+          break;
+          case ScanAway:
+            activity=;
+          break;
+          case ScanTurnaround:
+            activity=;
+          break;
+          case ScanBack:
+            activity=;
+          break;
+        }
+      }
+      
+    } else if(deadtime.hasFinished()) {//just expired
+        switch(activity){
+          case NeedsHoming:
+            activity=NeedsHoming;
+          break;
+          case HomeAway:
+            scan=0;
+            away=1;
+            endrun=inbound;
+          break;
+          case HomeBack:
+            activity=;
+          break;
+          case AtHome:
+            activity=;
+          break;
+          case ScanAway:
+            activity=;
+          break;
+          case ScanTurnaround:
+            activity=;
+          break;
+          case ScanBack:
+            activity=;
+          break;
+          
+        }
+    }
+      
+    
   //todo: if scanpass is not zero and we are not moving start moving
   //todo: if scanpass is not zero and we are moving then if related timer done reverse or stop
   //todo: other timers may expire and here is where we service them  
@@ -85,6 +156,9 @@ void loop() {
         break;
       case 'r':case 'R':
         scan = 1;
+        break;
+      case 'e':case 'E':
+        scan = 0;
         break;
       case 'l':case 'L':
         lights = 1;
