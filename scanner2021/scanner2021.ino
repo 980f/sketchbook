cda40767b82d4555538128e2851c2f51089a93c1
andyh <andy.980f@gmail.com>
2021-10-04 22:29:19 -0500

    break before make, and switch to table driven state machine

--- scanner2021/scanner2021.ino
+++ scanner2021/scanner2021.ino
@@ -1,185 +1,201 @@
 
 
+/*****
+   todo: pullup switch inputs
+   todo: count triggers and report on number of failed scans
+*/
+
 #include "chainprinter.h"
 ChainPrinter dbg(Serial, true); //true adds linefeeds to each invocation.
 
 //disappeared from libs: #include "cheaptricks.h"
 //we will want to delay some activities, such as changing motor direction.
 #include "millievent.h"
-
 #include "digitalpin.h"
 
-//switches on leadscrew/rail: 
+//switches on leadscrew/rail:
 DigitalInput nearend(2, LOW);
 DigitalInput farend(3, LOW);
-
+DigitalInput trigger(5, LOW);
 
 #include "edgyinput.h"
 EdgyInput homesense(farend);
 EdgyInput awaysense(nearend);
 
-//2 for the motor
-DigitalOutput scan(17);
-DigitalOutput away(18);
+//and how do we deal with trigger?
+
 //lights are separate so that we can force them on as work lights, and test them without motion.
 DigitalOutput lights(16);
+//2 for the motor
+DigitalOutput scan(17); //a.k.a run
+DigitalOutput away(18); //a.k.a direction
+
 //not yet sure what else we will do
 DigitalOutput other(19);
-
 //////////////////////////////////////////////////
-//these should probably come from eeprom
-bool idleNear;   //if true then idle near the motor, else idle at far end.
-MilliTick outbound;//time from starting moving scanner bar away from motor to stopping.
-MilliTick inbound;//time from starting moving scanner bar towards motor until turn around
+//finely tuned parameters
+static const MilliTick Breaker = 75;  //time to ensure one relay has gone off before turning another on
+static const MilliTick Maker = 100;   //time to ensure relay is on before trusting that action is occuring.
+static const MilliTick Fullrail = 10000; //timeout for homing, slightly greater than scan
+static const MilliTick Scanout = 9000;  //time from start to turnaround point
+static const MilliTick Scanback = 9200; //time from turnaround back to home, different from above due to hysterisis in both motion and the home sensor
+static const MilliTick TriggerDelay = 5310; //taste setting, time from button press to scan start.
+//////////////////////////////////////////////////
 
-//timers for out and in bound moves
-Monostable endrun;// when true quit moving
-Monostable deadtime;// when true start action for current state
-//state of motion
-bool haveHomed=0; //until we have activated home switch we don't know where we are
-bool inMotion=0;   //run relay should be on
-bool movingAway=0; //moving away from motor
+struct StateParams {
+  bool lights;
+  bool scan;
+  bool away;
+  MilliTick delaytime;
+};
+
+StateParams stab[] {
+  {0, 0, 0, Breaker}, //powerup
+  {0, 1, 0, Fullrail}, //homing
+  {0, 0, 1, BadTick}, //idle
+  {1, 0, 1, TriggerDelay}, //triggered  delay is from trigger active to scan starts
+  {1, 1, 1, Scanout}, //run away
+  {1, 0, 0, Breaker}, //stopping  //can turn both off together, no race
+  {1, 1, 0, Scanback}, //running home
+  {1, 0, 0, BadTick}, //got home  //!!! actually means didn't get all the way home!
+  {1, 0, 0, Breaker}, //home_early  //actually this is the prefered/normal way to get home
+  {0, 0, 1, Maker}, //chill, ignores trigger until this expires
+  //we go to idle from above
+  {0, 0, 0, BadTick}, //unused/guard table
+
+};
+
 
 //program states, above are hardware state
 enum MotionState {
-  NeedsHoming=0,
-  HomeAway,
-  HomeBack,
-  AtHome,
-  ScanAway,
-  ScanTurnaround,
-  ScanBack,
+  powerup = 0, //natural value for power up of program
+  homing,
+  idle,
+  triggered,
+  run_away,
+  stopping,
+  running_home,
+  got_home,
+  home_early,
+  chill,
+
+  oopsiedaisy //should never get here!
 };
 
-//set to non-zero when should be scanning. When decremented to zero motion stops once home.
-unsigned scanpass=0;
-MotionState activity=NeedsHoming;
+class MotionManager {
+    MonoStable timer;
+    MotionState activity;
+  public:
+    //if we need multiple passes: unsigned scanpass = 0;
+    void enterState(MotionState newstate) {
+      StateParams &params = stab[newstate];
+      scan = params.scan;
+      away = params.away;
+      timer = params.delaytime;
+      activity = newstate;
+    }
 
-//we want to avoid current spikes and any jerking from relay response time differenceses
-void enterState(MotionState newstate,MilliTick motiontimeout){
-  scan = 0; //always halt before any potential change of direction. Since most calls change direction we don't bother checking.      
-  deadtime = 50; //todo:  measure worst of motion stop and relay skew.
-  endrun = motiontimeout;
-  activity = newstate;
-}
+    void onTick() {
+      //read event inputs once for programming convenience
+      bool amdone = timer.hasFinished(); //which disables timer
+      bool amhome = homesense;
+
+      switch (activity) {
+        case idle: //must be here to honor trigger
+          if (trigger) {
+            enterState(triggered);
+          }
+          //todo: if not at home we are fucked?
+          break;
+        case running_home:
+          if (amhome) { //then we are done with this part of the effect.
+            enterState(home_early);//todo: rename state
+          } else if (amdone) {//we got stuck in the middle
+            enterState(homing);
+            //todo: deal with this error!
+            dbg("Short!");
+          }
+          break;
+
+        case homing:
+          if (amhome) {
+            enterState(idle);
+          } else if (amdone) {
+            //we got stuck in the middle
+            enterState(oopsiedaisy);//todo: deal with this error!
+          }
+          break;
+        default:
+          if (amdone) { //only true if timer was both relevant and has completed
+            enterState(activity + 1);
+          }
+          break;
+      }
+    }
+};
+
+MotionManager Motion;
+
+////////////////////////////////////////////////
 
 void setup() {
-  //todo: read options from EEprom 
+  //todo: read options from EEprom
   Serial.begin(115200);
 }
 
-void loop() {  
-  if(MilliTicked){//nothing need be fast and the processor may be in a tight enclosure
-    if(!deadtime.isRunning()){
-        
-      if(endrun){
-        switch(activity){
-          case NeedsHoming:
-            activity=NeedsHoming;
-          break;
-          case HomeAway:
-            scan=0;      
-            deadtime=50;
-            endrun=inbound;
-            activity=HomeBack;
-          break;
-          case HomeBack:
-            activity=;
-          break;
-          case AtHome:
-            activity=;
-          break;
-          case ScanAway:
-            activity=;
-          break;
-          case ScanTurnaround:
-            activity=;
-          break;
-          case ScanBack:
-            activity=;
-          break;
-        }
-      }
-      
-    } else if(deadtime.hasFinished()) {//just expired
-        switch(activity){
-          case NeedsHoming:
-            activity=NeedsHoming;
-          break;
-          case HomeAway:
-            scan=0;
-            away=1;
-            endrun=inbound;
-          break;
-          case HomeBack:
-            activity=;
-          break;
-          case AtHome:
-            activity=;
-          break;
-          case ScanAway:
-            activity=;
-          break;
-          case ScanTurnaround:
-            activity=;
-          break;
-          case ScanBack:
-            activity=;
-          break;
-          
-        }
+void loop() {
+  if (MilliTicked) { //nothing need be fast and the processor may be in a tight enclosure
+    Motion.onTick();
+    //audio.onTick();
+    //todo: other timers may expire and here is where we service them
+#if 0
+    if (homesense.changed()) {
+      dbg(homesense ? 'H' : 'h');
     }
-      
-    
-  //todo: if scanpass is not zero and we are not moving start moving
-  //todo: if scanpass is not zero and we are moving then if related timer done reverse or stop
-  //todo: other timers may expire and here is where we service them  
-    if(homesense.changed()){
-      dbg(homesense?'H':'h');
+    if (awaysense.changed()) {
+      dbg(awaysense ? 'F' : 'f');
     }
-    if(awaysense.changed()){
-      dbg(awaysense?'F':'f');
-    }
-   
+#endif
   }
+
   //todo: if serial do some test thing
-  if(Serial){
-    auto key= Serial.read();
-    if(key>0){
-    dbg("key:",key);
-    switch(key){
-      case 'f': case 'F':
-        away=0;
-        break;
-      case 'b':case 'B':
-        away = 1;
-        break;
-      case 'r':case 'R':
-        scan = 1;
-        break;
-      case 'e':case 'E':
-        scan = 0;
-        break;
-      case 'l':case 'L':
-        lights = 1;
-        break;
-      case 'o':case 'O':
-        lights = 0;
-        break;
-      case 'q':case 'Q':
-        other = 1;
-        break;
-      case 'a':case 'A':
-        other = 0;
-        break;  
-      //////////////////////////////////////  
-      default: //any unknown key == panic
-        dbg(" panic!");
-        scan=0;
-        away=0;
-        lights=0;
-        break;
-    }
+  if (Serial) {
+    auto key = Serial.read();
+    if (key > 0) {
+      dbg("key:", key);
+      switch (key) {
+        case 'f': case 'F':
+          away = 0;
+          break;
+        case 'b': case 'B':
+          away = 1;
+          break;
+        case 'r': case 'R':
+          scan = 1;
+          break;
+        case 'e': case 'E':
+          scan = 0;
+          break;
+        case 'l': case 'L':
+          lights = 1;
+          break;
+        case 'o': case 'O':
+          lights = 0;
+          break;
+        case 'q': case 'Q':
+          other = 1;
+          break;
+        case 'a': case 'A':
+          other = 0;
+          break;
+        //////////////////////////////////////
+        default: //any unknown key == panic
+          dbg(" panic!");
+          scan = 0;
+          away = 0;
+          lights = 0;
+          break;
+      }
     }
   }
 }
