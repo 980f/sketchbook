32d2b71b0495c74efaa1fcdcde8cb76d846c4389
andyh <andy.980f@gmail.com>
2021-10-05 16:37:25 -0500

    many little bugs quashed, looks good for testing with live hardware

--- flicker2021/flicker2021.ino
+++ flicker2021/flicker2021.ino
@@ -1,5 +1,12 @@
 #include "chainprinter.h"
+#if 1 //8684/606 with debug 7616/483 without, so around 1k of debug statments 
 ChainPrinter dbg(Serial, true); //true adds linefeeds to each invocation.
+//eventually these will be made editable by Serial
+const bool dbgi2c = 0;
+const bool dbgpin = 0;
+#else
+#define dbg(...)
+#endif
 
 #include "millievent.h"
 
@@ -14,7 +21,7 @@ struct Flickery {
     range[1] = ontime;
   }
 
-  void setup() {
+  virtual void setup() {
     be(true);//autostart
     duration = 750;//value chosen for debug
   }
@@ -44,20 +51,26 @@ struct FlickeryPin: public Flickery {
   FlickeryPin(int apin, unsigned ontime, unsigned offtime): Flickery(ontime, offtime), Led_Pin(apin) {
     //nada.
   }
-  virtual void be(bool on) {
+
+  void setup() {
+    //set pin as output
+    pinMode(Led_Pin, OUTPUT);
+    Flickery::setup();
+  }
+
+  void be(bool on) {
     Flickery::be(on);
     digitalWrite(Led_Pin, on ? HIGH : LOW);
-    dbg("Pin ", Led_Pin, '=', on ? "HIGH " : "LOW ");
+    if (dbgpin) dbg("Pin ", Led_Pin, '=', on ? "HIGH " : "LOW ");
   }
 };
 
-FlickeryPin led[] = {
-  {13, 1201, 1400}, //for debug
+const FlickeryPin led[] = {
   //station 4 will use one of these, scanner hallway two, station 9 all four but copied over I2C.
   { 10, 1201, 1400},
   { 9, 1302, 1500},
   { 8, 1100, 1703},
-  { 7, 1204, 1500},
+  { 7, 1204, 1325},
 
 };
 
@@ -70,21 +83,24 @@ void freezeAll() {
 
 }
 
-//import PCF7475 class here and pack the led array into it.
 #include "pcf8574.h"
 
-PCF8574 station9(0);//9th status panel, operand is jumper settings
+PCF8574 station9(0, 0, 100); //for 9th status panel, operand is jumper settings, bus 0, 100 kHz is its max bus rate
 
 void packout() {
   unsigned newly = 0;
   for (unsigned count = numleds; count-- > 0;) {
     if (led[count].ison) {
-      newly |= 3 << (1 + 2 * count); //this is how a particular board is wired, setting pairs of pins in case we need to boost drive.
+      newly |= 3 << (2 * count); //this is how a particular board is wired, setting pairs of pins in case we need to boost drive.
+      //dbg("Packing:",HEXLY(newly));
     }
   }
+  newly &= 0xFF; //only use lower 4 leds. Else we get perpetual miscompares.
   if (station9.seemsOk()) {//deferred test for debug
-    if (station9.cachedBits() != newly) {
+    auto cached = station9.cachedBits();
+    if (cached != newly) {
       station9 = newly; //this outputs to I2C
+      if (dbgi2c) dbg("I2C:", HEXLY(newly), " from:", HEXLY(cached));
     }
   }
 }
@@ -96,10 +112,12 @@ void setup() {
     led[count].setup();
   }
   dbg("setup I2C");
-  station9.begin(400000);
+  station9.setInput(0);//no inputs
+  station9.begin();
 
   if (station9.isPresent()) {
     packout();
+    dbg("I2C worked");
   } else {
     dbg("I2C failed");
   }
