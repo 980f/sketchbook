8e165394ad00ad8c520bcf860db89566dd9f33f4
andyh <andy.980f@gmail.com>
2022-03-01 12:37:54 -0600

    mostly working

--- wifi2812/wifi2812.ino
+++ wifi2812/wifi2812.ino
@@ -12,6 +12,7 @@
 
 #define LED_PIN 2
 
+
 /*
   other pin preferences:
   D1:SCL
@@ -33,10 +34,21 @@ StopWatchCore showTimer(false, true); //2nd true->micros
 Histogrammer<100>::ShowOptions showopts{true, true, 3000};
 
 #include "chainprinter.h"
-ChainPrinter dbg(Serial);
+ChainPrinter dbg(Serial, true);
+
+//esp puts some instruction set macros in global namespace!:
+#undef cli
 
 #include "clirp.h"
 CLIRP cli;
+
+#include "digitalpin.h"
+
+DigitalOutput blinker(LED_BUILTIN);
+
+DigitalInput beDark(D5, LOW);
+
+bool isDark = false;
 
 void showem() {
   showTimer.start();
@@ -57,17 +69,41 @@ void myCustomShow(void) {
     showem();
   }
 }
+/////////////////////////////////////////////
+struct FxTriplet {
+  uint8_t mode = FX_MODE_SPARKLE;
+  uint8_t brightness = 100;
+  uint16_t speed = 200;
+  void show(ChainPrinter &dbg) {
+    dbg("\tmode:", mode, ' ', _names[mode]);
+    dbg("\tbrightness:", brightness);
+    dbg("\tspeed:", speed );
+  }
+
+  void apply() {
+    ws2812fx.setBrightness(brightness);
+    ws2812fx.setSpeed(speed);
+    ws2812fx.setMode(mode);
+  }
+} fx[2];
+
 
 /////////////////////////////////////////////
 void setup() {
   Serial.begin(115200);
-  dbg("\nWifilights");
-
   ws2812fx.init();
-  ws2812fx.setBrightness(100);
-  ws2812fx.setSpeed(200);
-  ws2812fx.setMode(FX_MODE_RAINBOW_CYCLE);
+  isDark = !beDark; //force apparent change
 
+  fx[0].mode = FX_MODE_RAINBOW ;
+  fx[0].brightness = 100;
+  fx[0].speed = 200;
+
+  fx[1].mode = FX_MODE_SPARKLE;
+  fx[1].brightness = 100;
+  fx[1].speed = 200;
+
+
+  fx[1].apply();
   // set the custom show function
   ws2812fx.setCustomShow(myCustomShow);
 
@@ -81,16 +117,69 @@ void setup() {
 }
 
 
-void loop() {
-  if (MilliTicker) {
-    ws2812fx.service();
-    //
-    //    if (longest < elapsed) {
-    //      longest = elapsed;
-    //      Serial.println(elapsed);
-    //    }
-    //    if ((lastChecked % 5000) == 0 ) {
-    //      Serial.println(elapsed);
-    //    }
+void clido(int key) {
+
+  bool isUpper = key <= 'Z'; //can't remember asrduino name for isupper.
+  switch (tolower(key)) {
+    case ' ':
+      dbg("1/Dark:");
+      fx[1].show(dbg);
+      dbg("0/Light:");
+      fx[0].show(dbg);
+      dbg("Active:", isDark ? "Dark" : "Light");
+      break;
+    case 'm':
+      ws2812fx.setMode(fx[beDark].mode = cli.arg);
+      break;
+    case 's':
+      ws2812fx.setSpeed(fx[beDark].speed = cli.arg);
+      break;
+    case 'b':
+      ws2812fx.setBrightness(fx[beDark].brightness = cli.arg);
+      break;
+    case 'x':
+      { auto pack = dbg.stackFeeder(false);
+        dbg('\n');
+        for (unsigned di = 32; di-- > 0;) {
+          dbg((di & 3) ? ' ' : '\t', digitalRead(di));
+        }
+      }
+      break;
+    default:
+      dbg("Key:", key, " (", char(key), ")");
+      break;
   }
 }
+
+void checkcli() {
+  for (unsigned ki = Serial.available(); ki-- > 0;) {
+    auto key = Serial.read();
+    if (cli(key)) {
+      clido(key);
+    }
+  }
+}
+
+MonoStable toggler(2000);
+
+void loop() {
+  if (MilliTicker) {
+
+    if (changed(dbg.stifled, !bool(Serial))) {
+      dbg("\nWifilights  ");
+    }
+
+    ws2812fx.service();
+   
+    checkcli();
+    
+    if (changed(isDark, beDark)) {
+      dbg("Switching to mode:", isDark ? "Dark" : "Light");
+      fx[isDark].apply();
+    }
+
+    blinker = bool(Serial);
+    
+  }
+}
+//EOF.
