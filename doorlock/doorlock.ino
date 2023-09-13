eb9bf83070ea73c778a6227adadd265d6db9936f
andyh <andy.980f@gmail.com>
2023-09-13 09:45:47 -0500

    moved application guts into own class, after full debug will give it its own file

--- doorlock/doorlock.ino
+++ doorlock/doorlock.ino
@@ -15,6 +15,7 @@
 
 #include "edgypin.h"    //980F debounced digital inputs
 #include "motorshield.h" //the doorlock mechanism needs bipolar drive
+using Moveit =   L298Bridge::Code ; // L298Bridge might get replaced with L293 in a more generic build
 
 #include "millievent.h"  //we use explicit timers
 
@@ -27,102 +28,119 @@
 #define pulseclose 4
 #endif
 
-
-//we debounce all inputs as we don't want to rapidly dick with the mechanism
-
-//this is state input, not toggle, for when you can't see the mechanism. It works well with hardware limit switches in series between the arduino and the motor driver.
-//it is also the powerup state, it is checked at configuration time and executed.
-EdgyPin stable(7, LOW, 15);
-//this is toggle, used when you can see the mechanism and can press again if it is going in the wrong direction
-EdgyPin toggler(6, LOW, 30);
-//two buttons, safest approach whenever you can manage it.
-EdgyPin pulseopener(pulseopen, LOW, 30);
-EdgyPin pulsecloser(pulseclose, LOW,  30);
-
 //maximum motor run time, from last command. If you issue new commands while running it will continue to run.
 MilliTick timeout(300);
 
-//eventually we will add other driver options, here is where we will ifdef them
-using Moveit =   L298Bridge::Code ; // 'Code' was too generic, L298Brdige might get replaced with L293 in a more generic build
+struct DoorLocker {
+  //we debounce all inputs as we don't want to rapidly dick with the mechanism
 
-struct PulsedMotor {
-  SeeedStudioMotorShield driver; //from motorshield.h
+  //this is state input, not toggle, for when you can't see the mechanism. It works well with hardware limit switches in series between the arduino and the motor driver.
+  //it is also the powerup state, it is checked at configuration time and executed.
+  EdgyPin stable;//(7, LOW, 15);
+  //this is toggle, used when you can see the mechanism and can press again if it is going in the wrong direction
+  EdgyPin toggler;//(6, LOW, 30);
+  //two buttons, safest approach whenever you can manage it.
+  EdgyPin pulseopener;//(pulseopen, LOW, 30);
+  EdgyPin pulsecloser;//(pulseclose, LOW,  30);
 
-  //after some time the drive level is dropped, either by going to hold state or disabling (todo: configuration flag to indicate which should be used vs recompiling code)
-  MonoStable holder; //from millievent.h
+  //eventually we will add other driver options, here is where we will ifdef them
 
-  Moveit lastCommand; //from L298 included in motorshield.h
+  struct PulsedMotor {
+    SeeedStudioMotorShield driver; //from motorshield.h
 
-  //call this from setup, or when configuration is dynamically changed
-  void begin(MilliTick backoff) {
-    *this =  stable.raw() ? Moveit::Forward : Moveit::Backward;
-    holder = backoff; //which also starts it
-  }
+    //after some time the drive level is dropped, either by going to hold state or disabling (todo: configuration flag to indicate which should be used vs recompiling code)
+    MonoStable holder; //from millievent.h
 
-  //this is how you move
-  void operator =(Moveit newstate) {
-    lastCommand = newstate;
-    driver = newstate;
-    holder.start();
-  }
+    Moveit lastCommand; //from L298 included in motorshield.h
 
-  /** if @param toggleit then if last issued command was a direction go in other direction, else if off go to hold */
-  void toggle (bool toggleit = true) {
-    if (toggleit && lastCommand != Moveit::Off) { //if off leave off, do not go to hold
-      *this = L298Bridge::reverseof(lastCommand);
+    //call this from setup, or when configuration is dynamically changed
+    void begin(MilliTick backoff) {
+      // *this =  stable.raw() ? Moveit::Forward : Moveit::Backward;
+      holder = backoff; //which also starts it
     }
+
+    //this is how you move
+    void operator =(Moveit newstate) {
+      lastCommand = newstate;
+      driver = newstate;
+      holder.start();
+    }
+
+    /** if @param toggleit then if last issued command was a direction go in other direction, else if off go to hold */
+    void toggle (bool toggleit = true) {
+      if (toggleit && lastCommand != Moveit::Off) { //if off leave off, do not go to hold
+        *this = L298Bridge::reverseof(lastCommand);
+      }
+    }
+
+    /** you must call this once every millisecond */
+    void onTick() {
+      if (holder) {//a MonoStable is true once when its time has elapsed
+        driver = Moveit::Off;
+      }
+    }
+
+
+  };//end PulsedMotor
+
+  PulsedMotor driver;
+
+  DoorLocker(MilliTick runtime = 300):
+    stable(7, LOW, 15)
+    , toggler(6, LOW, 30)
+    , pulseopener(pulseopen, LOW, 30)
+    , pulsecloser(pulseclose, LOW,  30)
+  {
+    driver.holder = runtime;
   }
 
-  /** you must call this once every millisecond */
+  void setup() {
+    stable.begin();
+    toggler.begin();
+    pulseopener.begin();
+    pulsecloser.begin();
+
+    driver.begin(timeout);
+  }
+
   void onTick() {
-    if (holder) {//a MonoStable is true once when its time has elapsed
-      driver = Moveit::Off;
+    driver.onTick();//for delayed shutoff.
+    //debounce all even if they will be overridden or ignored:
+    auto togglerticked = toggler.onTick();
+    auto stableticked = stable.onTick();
+    auto openerticked = pulseopener.onTick();
+    auto closerticked = pulsecloser.onTick();
+
+    //priority scan, only honor one per millitick. Simultaneous means some may get ignored.
+    if (togglerticked && toggler) { //tested first as the othes are idempotent, repeating them gets the same action.
+      driver.toggle(toggler);
+    } else if (openerticked && pulseopener) { //could drop the trailing term to fire on release as well as press
+      driver = Moveit::Forward;
+    } else if (closerticked && pulsecloser) {
+      driver = Moveit::Backward;
+    } else  if (stable.onTick()) {
+      driver = stable ? Moveit::Forward : Moveit::Backward; //if this is backwards swap the wires to the device :)
     }
   }
-};
+
+};//end Doorlocker
+
+DoorLocker locker;
+
 ////////////////////////////////////
 #include "sui.h" //Simple User Interface
 
 SUI sui(Serial, Serial);// default serial for in and out, first use of file is on a Leonardo
 
-
-////////////////////////////////////
-
-PulsedMotor driver;
-
 void setup() {
-  stable.begin();
-  toggler.begin();
-  pulseopener.begin();
-  pulsecloser.begin();
-
-  driver.begin(timeout);
-}
-
-void onTick() {
-  driver.onTick();//for delayed shutoff.
-  //debounce all even if they will be overridden or ignored:
-  auto togglerticked = toggler.onTick();
-  auto stableticked = stable.onTick();
-  auto openerticked = pulseopener.onTick();
-  auto closerticked = pulsecloser.onTick();
-
-  //priority scan, only honor one per millitick. Simultaneous means some may get ignored.
-  if (togglerticked && toggler) { //tested first as the othes are idempotent, repeating them gets the same action.
-    driver.toggle(toggler);
-  } else if (openerticked && pulseopener) { //could drop the trailing term to fire on release as well as press
-    driver = Moveit::Forward;
-  } else if (closerticked && pulsecloser) {
-    driver = Moveit::Backward;
-  } else  if (stable.onTick()) {
-    driver = stable ? Moveit::Forward : Moveit::Backward; //if this is backwards swap the wires to the device :)
-  }
+  locker.setup();
 }
 
 void loop() {
   if (MilliTicker) {//true once per millisecond regardless of how often called
-    onTick();//moved code out of line for editing convenience
+    locker.onTick();
   }
+
   sui([](char key) {//the sui will process input, and when a command is present will call the following code.
     bool upper = key < 'a';
     switch (toupper(key)) {
@@ -130,18 +148,18 @@ void loop() {
         dbg("ignored:", unsigned(sui), ',', key);
         break;
       case ' '://status dump 0 is off, 1 or 2 is direction, 3 is 'hold/brake'
-        dbg(driver.lastCommand, '\t', driver.holder.due());
+        dbg(locker.driver.lastCommand, '\t', locker.driver.holder.due());
         break;
       case '[':
-        driver = Moveit::Forward;
+        locker.driver = Moveit::Forward;
         break;
       case ']':
-        driver = Moveit::Backward;
+        locker.driver = Moveit::Backward;
         break;
       case 'H': //holdoff time
-        driver.begin(timeout = sui);
+        locker.driver.begin(timeout = sui);
         break;
-        //todo: debounce times
+        //todo: config debounce times
     }
   });
 }
