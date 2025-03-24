9ed3996bd5091d3affaa0843c61899cf1b0471d7
andyh <andy.980f@gmail.com>
2025-03-24 18:21:01 -0500

    prep for using block.h

--- dwarvenMines/broadcastNode.h
+++ dwarvenMines/broadcastNode.h
@@ -1,39 +1,43 @@
 #pragma once
 
-#ifndef ESPNOW_WIFI_CHANNEL
-#warning "using 6 for wifi channel, #define ESPNOW_WIFI_CHANNEL before including this header if you don't like that"
-#define ESPNOW_WIFI_CHANNEL 6
+#ifndef BroadcastNode_WIFI_CHANNEL
+#warning "using 6 for wifi channel, #define BroadcastNode_WIFI_CHANNEL before including this header if you don't like that"
+#warning "and since you are new to this library you will likely want to pass BroadcastNode_Triplet as the constructor arg"
+#define BroadcastNode_WIFI_CHANNEL 6
 #endif
 
-#include "ESP32_NOW.h"
-#include "WiFi.h"
+#include <ESP32_NOW.h>
+#include <WiFi.h>
 
 #include <esp_mac.h>  // For the MAC2STR and MACSTR macros
 
-#define ESPNOW_Triplet ESPNOW_WIFI_CHANNEL, WIFI_IF_STA, nullptr
+#define BroadcastNode_Triplet BroadcastNode_WIFI_CHANNEL, WIFI_IF_STA, nullptr
 
 
 class BroadcastNode : public ESP_NOW_Peer {
-    static void new_node_thunk(const esp_now_recv_info_t *info, const uint8_t *data, int len, void *arg) {
-      reinterpret_cast<BroadcastNode *>(arg)->unknown_node(info, data, len);
+    static void new_node_thunk(const esp_now_recv_info_t *info, const uint8_t *data, int len, void *arg) {//declared by espressif
+      reinterpret_cast<BroadcastNode *>(arg)->unknown_node(info, len, data);
     }
 
   public:
     //debug control flags
-    bool spew = false;
-    bool errors = true;
+    static bool spew;// = false;
+    static bool errors;// = true;
 
-    BroadcastNode (uint8_t channel, wifi_interface_t iface, const uint8_t *lmk) : ESP_NOW_Peer(ESP_NOW.BROADCAST_ADDR,  channel, iface, lmk) {
+    BroadcastNode (uint8_t channel, wifi_interface_t iface, const uint8_t *lmk = nullptr) : ESP_NOW_Peer(ESP_NOW.BROADCAST_ADDR,  channel, iface, lmk) {
       //don't do anything on construction, so that we can statically construct.
     }
-    ~BroadcastNode () {}
+    //todo: remember that we added ourselves and remove ourselves, but we shouldn't be dynamically doing that so NYI.    ~BroadcastNode () {}
 
-    virtual bool validMessage(const uint8_t *data, size_t len) {
+    virtual bool validMessage(size_t len, const uint8_t *data) {
       return true;//default promiscuous
     }
 
     // Function to send a message to all devices within the network
-    bool send_message(const uint8_t *data, size_t len) {
+    bool send_message(size_t len, const uint8_t *data) {
+      if (len == 0 || data == nullptr) {
+        return true; //vacuous messages are instantly sent successfully
+      }
       if (send(data, len)) {
         return true;
       }
@@ -44,8 +48,9 @@ class BroadcastNode : public ESP_NOW_Peer {
     }
 
   protected:
+
     // Function to print the received messages from the master
-    void onReceive(const uint8_t *data, size_t len, bool broadcast = true) override {
+    void onReceive(const uint8_t *data, size_t len, bool broadcast = true) override {//declared by espressif
       if (spew) {
         if (broadcast) {
           Serial.printf("Received a broadcast message:\n");
@@ -57,17 +62,17 @@ class BroadcastNode : public ESP_NOW_Peer {
     }
 
   private:
-    // Callback called when an unknown peer sends a message
-    void unknown_node(const esp_now_recv_info_t *info, const uint8_t *data, int len) {
+    // called (via thunk) when an unknown peer sends a message
+    void unknown_node(const esp_now_recv_info_t *info, unsigned len, const uint8_t *data) {
       if (spew) {
-        if (memcmp(info->des_addr, ESP_NOW.BROADCAST_ADDR, 6) == 0) {
+        if (memcmp(info->des_addr, ESP_NOW.BROADCAST_ADDR, 6) == 0) {//todo: apply our MAC class
           Serial.printf("Node " MACSTR " sent a broadcast message\n", MAC2STR(info->src_addr));
         } else {
           Serial.printf("Received a unicast message from " MACSTR, MAC2STR(info->src_addr));
         }
       }
       //here is where we could qualify the peer and if its message indicates it is on our network than "add_peer" it and process the message.
-      if (validMessage(data, len)) {
+      if (validMessage(len, data)) {
         //todo: construct peer from info->src_addr and esp_now add that.
         onReceive(data, len, true);//message from nodes that are not added are not sent to onReceive by ESP library.
       }
@@ -77,7 +82,7 @@ class BroadcastNode : public ESP_NOW_Peer {
     bool begin(bool isLocal, bool isReceiveOnly) {
       int startupTime = - millis();
       WiFi.mode(WIFI_STA);
-      WiFi.setChannel(ESPNOW_WIFI_CHANNEL);
+      WiFi.setChannel(BroadcastNode_WIFI_CHANNEL);
       while (!WiFi.STA.started()) {//todo: hook events and get rid of this nominally infinite loop.
         delay(100);//measured: 100,191,257, seems to warmup and converge around 180. Seems to depend upon how many nodes are live.
       }
@@ -87,7 +92,7 @@ class BroadcastNode : public ESP_NOW_Peer {
       }
 
       if (errors) {
-        Serial.printf("Using Channel: %d\n", ESPNOW_WIFI_CHANNEL);
+        Serial.printf("Using Channel: %d\n", BroadcastNode_WIFI_CHANNEL);
         Serial.println("Own  MAC Address: " + WiFi.macAddress());
       }
 
@@ -108,3 +113,6 @@ class BroadcastNode : public ESP_NOW_Peer {
     }
 
 };
+
+bool BroadcastNode::spew = false;
+bool BroadcastNode::errors = true;
