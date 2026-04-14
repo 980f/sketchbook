e48a712f921225a4a42673b375c695bdd4a7a6ce
andyh <andy.980f@gmail.com>
2026-04-14 16:59:10 -0500

    oops, forgot one new file

---
+++ rgb_c3/rgb_driver.h
@@ -0,0 +1,33 @@
+#pragma once
+// todo: see if we have a more complete class somewhere, such as in FastLED.
+struct Color {
+  unsigned r;
+  unsigned g;
+  unsigned b;
+
+  void apply(char color, unsigned rawvalue) {
+    switch (color) {
+      case 'r':
+        r = rawvalue;
+        break;
+      case 'g':
+        g = rawvalue;
+        break;
+      case 'b':
+        b = rawvalue;
+        break;
+    }
+  }
+};
+
+/** access to the hardware */
+#include "ESP32Servo.h"
+struct RGB_C3 {
+  // todo: pick 3 pins
+  Color desired; // retain applied value for debug, and as a convenience for manipulation
+
+  void apply(Color &desired) {
+    // todo: send 'desired' values to hardware.
+    this->desired = desired;
+  }
+};
