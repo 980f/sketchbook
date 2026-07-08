618fa3eb92b818f98b6dbee4ba2c2400f01f017d
andyh <andy.980f@gmail.com>
2026-07-08 13:18:28 -0500

    derived from Blink, with massive changes

---
+++ pygamer/pygamer.ino
@@ -0,0 +1,60 @@
+#include "cheaptricks.h"
+#include <Mouse.h>
+/*
+  explore pygamer board
+*/
+
+using TimerTick = decltype(millis());
+#include "pinFlasher.h"
+Flasher flasher(LED_BUILTIN);
+
+///////////////////////////////////////
+#include "sui.h"
+struct MySui : public SUI<> {
+  using SUI::SUI; // incantation that is required to inherit base class constructors.
+
+  bool handleKey(unsigned char cmd, bool wasUpper) {
+    switch (tolower(cmd)) {
+    case '\r':
+    case '\n':
+      break;
+    case '!': {
+      flasher.adjust(cli[1],cli[0]);
+    } break;
+    case 'c':
+      flasher.showCount = wasUpper;
+      break;
+    case ' ': // show program state
+      flasher.dump();
+      break;
+    default: // unrecognized command stuff gets to here
+      cout("\nunknown command letter ", wasUpper ? toUpperCase(cmd) : cmd);
+      break;
+    }
+    return true; // discard
+  }
+} sui(Serial, Serial);
+
+TimerTick serialLive = 0; // takes around a second for USB serial to start working.
+TimerTick serialChecked = 0;
+///////////////////////////////////////
+void setup() {
+  Serial.begin(115200);
+  flasher.setup();
+}
+
+void loop() {
+  TimerTick currentMillis = millis();
+  flasher.loop(currentMillis);
+  if (!serialLive) {
+    if (changed(serialChecked, currentMillis)) {
+      if (Serial) { // reduce pressure on USB Serial code which takes a time to check if it is working.
+        serialLive = serialChecked;
+        Serial.printf("\nSerial started at %u", serialLive);
+      }
+    }
+  }
+  if (serialLive) { // don't check the user interface if it is not connected.
+    sui.loop();
+  }
+}
